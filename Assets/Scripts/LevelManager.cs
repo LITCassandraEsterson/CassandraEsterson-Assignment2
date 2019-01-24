a1d1534461cd170c063c73625508cfe6c79668e4
LITCassandraEsterson <46717288+LITCassandraEsterson@users.noreply.github.com>
2019-01-24 12:51:49 +0000

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public zoneScript zoneDetector;
+
+    public HingeJoint2D pivotHingeJoint;
+    private HingeJoint2D spinningHingeJoint;
+
+
+    void Update()
+    {
+        CheckForKeyboardInputs();
+    }
+
+    private void CheckForKeyboardInputs()
+    {
+        if (zoneDetector.zoneEntered)
+        {
+            JointMotor2D theMotor = pivotHingeJoint.motor;
+            theMotor.motorSpeed = 100f;
+            pivotHingeJoint.motor = theMotor;
+        }
+    }
+}
+
---
+++ Assets/zoneScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoneScript : MonoBehaviour {
+
+    public bool zoneEntered;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {       
+            zoneEntered = true;
+    }
+    
+}
